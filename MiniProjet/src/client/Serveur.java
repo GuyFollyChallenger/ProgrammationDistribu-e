7f281faa47ba94c0df907209c3ec5af1a0169835
Unknown <mgrkrr21@gmail.com>
2017-12-19 11:42:42 +0100

    modif

---
+++ MiniProjet/src/client/Serveur.java
@@ -0,0 +1,14 @@
+package client;
+
+public class Serveur {
+
+	public Serveur() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
